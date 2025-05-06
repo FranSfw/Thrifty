6593414b32c95f689f1e90b71509a1cab5c4834d
Francisco Perez <117410205+FranSfw@users.noreply.github.com>
2025-05-06 16:08:46 -0700

    update .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,16 +6,14 @@ import { User } from "./entities/user";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "aws-0-us-west-1.pooler.supabase.com",
-  port: 6543,
-  username: "postgres.agetxtbbhhdzkssdrldg",
-  password: "LuisUxiono33",
-  database: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: String(process.env.DB_PASSWORD),
+  database: process.env.DB_NAME,
   synchronize: true, // En desarrollo puede ser true, pero en producción debería ser false
-  logging: false,
+  logging: true,
   entities: [Branch, Log, Product, User],
-  migrations: ["src/migration/**/*.ts"],
-  subscribers: ["src/subscriber/**/*.ts"],
 });
 
 export const initializeDataSource = async () => {
